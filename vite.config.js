9d5b5cf5970f4a8825820773e19ed4c6ab51cc65
Igor <eng.igorwatanabe@gmail.com>
2023-08-04 14:23:48 -0300

    mudando para react

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/igorwatanabe.github.io/",
+  base: "/portfolio-web",
   plugins: [react()],
 })
