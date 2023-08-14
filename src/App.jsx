c2541fa78059b7439b06ca96488458abb3787931
Igor <eng.igorwatanabe@gmail.com>
2023-08-14 17:37:48 -0300

    corrigindo rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import './App.css';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
@@ -8,11 +8,11 @@ function App() {
 
   return (
     <div>
-      <Switch>
+      <HashRouter>
         <Route exact path="/portfolio-web" component={ Home } />
         <Route exact path="/portfolio-web/projects" component={ Projects } />
         <Route exact path="/portfolio-web/contact" component={ Contact } />
-      </Switch>
+      </HashRouter>
     </div>
   )
 }
