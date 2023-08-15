cf0ee74bf99bb7f6b781c13beb8afa175b59b4fe
Igor <eng.igorwatanabe@gmail.com>
2023-08-14 22:41:52 -0300

    corrigindo rotas

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,21 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/portfolio-web",
-  plugins: [react()],
-})
-
-// export default defineConfig(({ command }) => {
-//   const config = {
-//     plugins: [react()],
-//     base: '/',
-//   }
-
-//   if (command !== 'serve') {
-//     config.base = '/portfolio-web/'
-//   }
-
-//   return config
+// export default defineConfig({
+//   base: "/portfolio-web",
+//   plugins: [react()],
 // })
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/portfolio-web',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/portfolio-web/'
+  }
+
+  return config
+})
 
