9f9b91b70080dad7e05c01172609d98b9d64f47e
Igor <eng.igorwatanabe@gmail.com>
2023-08-14 23:19:50 -0300

    corrigindo rotas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import brasao from '../assets/brasao-watanabe.png';
 
 function BasicExample() {
@@ -23,7 +24,7 @@ function BasicExample() {
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
           <Nav activeKey="/home">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/projects">Projects</Nav.Link>
+            <Link to="/projects">Projects</Link>
             <Nav.Link href="/contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
